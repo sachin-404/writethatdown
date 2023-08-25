907f7bbe411332841bfa70f3daa9c441a3b20a73
Sachin Kant <sachinkant404@gmail.com>
2023-08-26 02:15:50 +0530

    setup database connection

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+
+load_dotenv()
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+DB_CON_KEY = os.getenv('DB_KEY')
+db_connection = MongoClient(DB_CON_KEY)
+
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
+async def read_item(request: Request):
+    docs = db_connection.notes.notes.find({})
+    for doc in docs:
+        print(doc)
     return templates.TemplateResponse("index.html", {"request": request})
